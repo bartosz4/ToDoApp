010d2110758927cc7a48fa466b49855237170feb
bartosz4 <bartosz44@gmail.com>
2019-05-06 13:25:27 +0200

    Added some files for app

---
+++ src/components/AddTask.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class AddTask extends Component {
+  state = {};
+  render() {
+    return <div>
+
+xcvbn
+
+
+    </div>;
+  }
+}
+
+export default AddTask;
