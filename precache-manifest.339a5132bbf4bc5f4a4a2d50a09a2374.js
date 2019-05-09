a188c94a99562a0b77861fa374669a54d6c120d2
bartosz4 <bartosz44@gmail.com>
2019-05-09 13:04:11 +0200

    Updates

---
+++ precache-manifest.339a5132bbf4bc5f4a4a2d50a09a2374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1157e0d80681ea48451e028c2ec87981",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "0712acbcab8ba96e06e8",
+    "url": "/ToDoApp/static/css/main.5b5d176d.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/ToDoApp/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "0712acbcab8ba96e06e8",
+    "url": "/ToDoApp/static/js/main.dac9da89.chunk.js"
+  },
+  {
+    "revision": "fb1067834985d33aa2a1",
+    "url": "/ToDoApp/static/js/runtime~main.f2c45213.js"
+  },
+  {
+    "revision": "d29cf20b73c3e7a3fd1681be60850162",
+    "url": "/ToDoApp/static/media/wall.d29cf20b.jpg"
+  }
+]);
